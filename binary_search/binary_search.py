28574884b863bda2fbfb17b64ceb6a2c68f4403e
Casey Williams <williamshoops96@gmail.com>
2017-12-08 11:52:10 +0000

    Add files via upload

---
+++ binary_search/binary_search.py
@@ -0,0 +1,39 @@
+# Binary search algorithms
+
+def binary_search(arr, item):
+    low = 0
+    high = len(arr)
+
+    while low <= high:
+        mid = low + (high - low) / 2
+
+        if arr[mid] == item:
+            return mid
+        elif arr[mid] < item:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return Exception("Item not found")
+
+
+## needs to be fixed
+def recursive_binary_search(arr, item):
+    low = 0
+    high = len(arr)
+    mid = low + (high - low) / 2
+
+    if arr[mid] == item:
+        return mid
+    elif arr[mid] < item:
+        return recursive_binary_search(arr[mid+1:high], item)
+    else:
+        return recursive_binary_search(arr[low:mid], item)
+
+    return Exception("Item not found")
+
+
+my_arr = [1, 3, 5, 7, 8, 11, 15, 1229]
+
+print binary_search(my_arr, 1229)
+#print recursive_binary_search(my_arr, 15)
